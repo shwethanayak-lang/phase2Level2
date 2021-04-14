b1190d1e954522f4f628d06aa0b1b6f6a5c47392
Shwetha Nayak <64678982+shwethanayak-lang@users.noreply.github.com>
2021-04-14 15:59:32 +0530

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -3,21 +3,28 @@ buttons = document.querySelectorAll("button");
 let screenValue = " ";
 
 for (item of buttons) {
+  // add event handler
   item.addEventListener("click", (e) => {
     buttonText = e.target.innerText;
     console.log(buttonText);
-
+    // clear function for "C" button
     if (buttonText == "C") {
       screenValue = " ";
       screen.value = screenValue;
-    } else if (buttonText == "=") {
+    }
+    // function of '=' sign
+    else if (buttonText == "=") {
       screen.value = eval(screenValue);
-    } else if (buttonText == "CE") {
+    }
+    // work of 'CE'
+    else if (buttonText == "CE") {
       var number = screenValue;
       var len = number.length - 1;
       var newnumber = number.substring(0, len);
       screen.value = newnumber;
-    } else {
+    }
+    //Code for any buttons other than above
+    else {
       screenValue += buttonText;
       screen.value = screenValue;
     }
